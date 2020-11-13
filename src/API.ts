7a51d0006160a4bfc8a94c4dab2bdec2b4bc647c
ahmedali8 <imahmedalibhatti@gmail.com>
2020-11-13 18:30:55 +0500

    fixed returning value to array

--- src/API.ts
+++ src/API.ts
@@ -20,7 +20,7 @@ export enum Difficulty {
 export const fetchQuizQuestions = async (
   amount: number,
   difficulty: Difficulty
-): Promise<QuestionState> => {
+): Promise<QuestionState[]> => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
 
   const data = await (await fetch(endpoint)).json();
