483c467e83c6861bdac7d82dba9552891b539c8a
ahmedali8 <imahmedalibhatti@gmail.com>
2020-11-13 18:30:26 +0500

    fixed the props and added key to map

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 type QuestionCardProps = {
   question: string;
-  answer: string[];
+  answers: string[];
   callback: any;
   userAnswer: any;
   questionNr: number;
@@ -11,7 +11,7 @@ type QuestionCardProps = {
 
 const QuestionCard: React.FC<QuestionCardProps> = ({
   question,
-  answer,
+  answers,
   callback,
   userAnswer,
   questionNr,
@@ -24,9 +24,9 @@ const QuestionCard: React.FC<QuestionCardProps> = ({
       </p>
       <p dangerouslySetInnerHTML={{ __html: question }} />
       <div>
-        {answer.map((answer) => (
-          <div>
-            <button disabled={userAnswer} onClick={callback}>
+        {answers.map((answer, index) => (
+          <div key={index}>
+            <button disabled={userAnswer} value={answer} onClick={callback}>
               <span dangerouslySetInnerHTML={{ __html: answer }} />
             </button>
           </div>
